546f4fa339a324f71079714eebd9d36bf2ebf657
Abdelrahman Reyad <92989840+Abdelrahman-7z7@users.noreply.github.com>
2025-10-01 18:46:01 +0300

    changing the place of the home page to the index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,7 +8,9 @@ export default function Index() {
       <Image source={images.bg} className="w-full absolute z-0"/>
 
       {/* applying "scrollView" which make the whole screen scrollable */}
-      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{
+      <ScrollView className="flex-1 px-5" 
+        showsVerticalScrollIndicator={false}  //hides the scrol bar
+        contentContainerStyle={{ //define the scroll high of the whole page
         minHeight: "100%", paddingBottom: 10
       }}>
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
