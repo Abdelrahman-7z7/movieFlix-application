7183de4121d1abeb43e8f7e7a4e9dfefb9d2d5c6
Abdelrahman Reyad <92989840+Abdelrahman-7z7@users.noreply.github.com>
2025-10-20 20:11:36 +0300

    adding the onChenge and value and editable props to handle the states of the searchbar

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,9 +5,12 @@ import { Image, TextInput, View } from 'react-native'
 interface Props {
   placeholder: string
   onPress?: () => void
+  value?: string
+  onChangeText?: (text: string) => void
+  editable?: boolean
 }
 
-const SearchBar = ({ onPress, placeholder }: Props) => {
+const SearchBar = ({ onPress, placeholder, value, onChangeText, editable = true }: Props) => {
   return (
     <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4">
       <Image
@@ -17,10 +20,11 @@ const SearchBar = ({ onPress, placeholder }: Props) => {
       <TextInput
         onPress={onPress}
         placeholder={placeholder}
-        value=""
-        onChangeText={() => {}}
+        value={value}
+        onChangeText={onChangeText}
         placeholderTextColor="a8b5db"
         className="flex-1 ml-2 text-white"
+        editable={editable}
       ></TextInput>
     </View>
   )
